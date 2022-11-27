ffdb10726523252dc2caff3df241fa2045f46ba6
Marcos Plata <marcosplata98@gmail.com>
2022-11-27 07:40:20 -0500

    migrate the controllers

--- src/tasks/tasks.services.js
+++ src/tasks/tasks.services.js
@@ -15,25 +15,52 @@ const getAllTodos = (req, res) => {
             res.status(400).json({message: err.message})
         })
 }
+/* 
+const getAllTodosWithTryCatch = (req, res) => {
+    try {
+        const data = taskControllers.findAllTodos()
+        res.status(200).json(data)
+        return data
+
+    } catch(err) {
+        res.status(400).json({message: err.message})
+        return err
+    }
+}
+ */
 
 const getTodoById = (req, res) => {
     const id = req.params.id
-    const data = taskControllers.findTodoById(id)
-    if(data) {
-        res.status(200).json(data)
-    } else {
-        res.status(404).json({message: 'Invalid ID'})
-    }
+    taskControllers.findTodoById(id)
+        .then((data) => {
+            if(data) {
+                res.status(200).json(data)
+            } else {
+                res.status(404).json({message: 'Invalid ID'})
+            }
+        })
+        .catch(err => {
+            res.status(400).json({message: err.message})
+        }) 
 }
 
 const postTodo = (req, res) => {
     const {title, description} = req.body
+    taskControllers.createTodo({title, description})
+        .then((data) => {
+            res.status(201).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({message: err.message})
+        })
+    /* 
     if(title && description) {
         const data = taskControllers.createTodo({title, description})
         res.status(201).json(data)
     } else {
         res.status(400).json({message: 'Invalid Data'})
     }
+     */
 }
 
 module.exports = {
